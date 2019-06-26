13e0bde81c4036d78706d47572170103bc070722
Daniel hernandez <00209317@uca.edu.sv>
2019-06-25 21:28:32 -0600

    add changes

--- models/coin.js
+++ models/coin.js
@@ -1,13 +1,6 @@
 var mongoose = require('mongoose')
 var coin = new mongoose.Schema({
-    name : String,
-    country: String,
-    value: Number,
-    value_us: Number,
-    year: Number,
-    review: String,
-    isAvailable: Boolean        ,
-    img:String
+    name : String
 });
 
 module.exports = mongoose.model("coin",coin);
